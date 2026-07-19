9bc022fe0d59ea1e7368c9d771820a98d695cc86
germk0 <germanberti.022@gmail.com>
2026-07-19 01:10:50 -0400

    Update twrp_9032Z.mk

--- twrp_9032Z.mk
+++ twrp_9032Z.mk
@@ -1,7 +1,6 @@
-$(call inherit-product, thrown/core/product_configured.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := 9032Z
 PRODUCT_NAME := twrp_9032Z
